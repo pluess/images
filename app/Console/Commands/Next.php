85a314171f22b403d6cf590d52c31c7303da1d9a
pluess <ernst@pluess.li>
2017-11-21 17:52:12 +0100

    evaluate next image

---
+++ app/Console/Commands/Next.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Image;
+use Illuminate\Console\Command;
+
+class Next extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'images:next';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Finds the next image to show.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        /** @var Image */
+        $image = Image::next();
+        $this->info('Next image is ' . $image->path);
+    }
+}
--- app/Image.php
+++ app/Image.php
@@ -9,6 +9,8 @@ use \RecursiveIteratorIterator;
 class Image extends Model
 {
 
+    const BEARBEITET = '_bearbeitet';
+
     protected $fillable = ['path'];
 
     /**
@@ -39,6 +41,45 @@ class Image extends Model
         }
 
         return $counter;
+    }
+
+    /**
+     * Finds a random image from the DB and marks it as used.
+     *
+     * The one with used=true and the latest updated_at field is
+     * the current image to show.
+     *
+     * @return Image
+     */
+    public static function next()
+    {
+        /** @var Image */
+        $image = Image::where('path', 'not like', '%' . self::BEARBEITET . '%')
+            ->where('used', false)
+            ->get()
+            ->random();
+
+        $image->used = true;
+        $image->save();
+
+        // is there a _bearbeitet version?
+        /** @var string */
+        $pathWithBearbeitet = str_replace('.', self::BEARBEITET . '.', $image->path);
+        /** @var Image */
+        $imageWith = Image::where('path', $pathWithBearbeitet)
+            ->get()
+            ->first();
+
+        if ($imageWith) {
+            $imageWith->used = true;
+            // make sure the bearbeitet version is the latest
+            sleep(1);
+            $imageWith->save();
+            $image = $imageWith;
+        }
+
+        return $image;
+
     }
 
 }
